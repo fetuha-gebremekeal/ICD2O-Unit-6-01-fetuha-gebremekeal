843194c11a95caf6142737f1d04aebed275d3c17
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-05-08 18:36:47 +0000

    js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Fetuha All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Fetuha
+// Created on: May 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+function findIntegerClicked() {
+  const integer = parseInt(document.getElementById("num-entered").value)
+  let answer = 0
+  for (let counter = 1; counter <= integer; counter++) {
+    answer += Math.abs(counter)
+    console.log(counter)
+  }
+  document.getElementById("answer").innerHTML = "Your answer is: " + answer
+}
+
