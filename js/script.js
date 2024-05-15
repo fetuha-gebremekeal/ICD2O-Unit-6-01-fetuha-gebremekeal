f97db6af860f4785a5c932b5f7b6f2da5234ca30
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-05-15 18:29:02 +0000

    js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,15 @@
 // This file contains the JS functions for index.html
 "use strict"
 
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+    scope: "/ICS2O-PWA-Test/",
+  })
+}
+
 function findIntegerClicked() {
   const integer = parseInt(document.getElementById("num-entered").value)
   let answer = 0
